85ee96f8af6f6c4b492c2d475318a31a674b5112
Jeremiah Azilaku <azilakujeremiah@gmail.com>
2023-11-25 12:57:31 +0000

    Add project files.

---
+++ FormulaOneApp/Controllers/TeamsController.cs
@@ -0,0 +1,91 @@
+﻿using FormulaOneApp.Data;
+using FormulaOneApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FormulaOneApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeamsController : ControllerBase
+    {
+        public TeamsController(AppDbContext context)
+        {
+            this._context = context;
+        }
+
+        //private static List<Team> teams = new List<Team>()
+        //{
+        //    new Team() { Id = 1,Country="Germany",Name="Mercedes AMG F1", TeamPrinciple="Toto Wolf"},
+        //    new Team() { Id = 2,Country="Italy", Name="Ferrari", TeamPrinciple="Mattia"},
+        //    new Team() { Id = 3,Country="Swiss", Name="Alpha Romeo", TeamPrinciple="Freideric Vasseur"}
+        ////};
+
+        private readonly AppDbContext _context;
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var teams = await _context.Teams.ToListAsync();
+
+            return Ok(teams);
+        }
+
+        [HttpGet(template: "{id:int}")]
+        public async Task<IActionResult> Get(int id) {
+
+            var team = await _context.Teams.FirstOrDefaultAsync(x => x.Id == id);
+
+            if(team == null)
+            {
+                return BadRequest("Invalid Id");
+            }
+
+            return Ok(team);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Team team)
+        {
+            if(team != null)
+            {
+               await _context.Teams.AddAsync(team);
+                await _context.SaveChangesAsync();
+            }
+
+            return CreatedAtAction(nameof(Get), new {id = team?.Id }, team);
+        }
+
+        [HttpPatch]
+        public async Task<IActionResult> Patch(int i, string country)
+        {
+            var team = await _context.Teams.FirstOrDefaultAsync(x => x.Id == i);
+
+
+            if (team == null)
+            {
+                return BadRequest("Invalid Id");
+            }
+
+            team.Country = country;
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var team = await _context.Teams.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (team == null)
+            {
+                return NotFound();
+            }
+
+            _context.Teams.Remove(team);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ FormulaOneApp/Program.cs
@@ -0,0 +1,56 @@
+using FormulaOneApp.Configurations;
+using FormulaOneApp.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(options => 
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.Configure<JwtConfig>(builder.Configuration.GetSection("JwtConfig"));
+
+builder.Services.AddAuthentication(options => {
+    options.RequireAuthenticatedSignIn = true;
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(jwt =>
+{
+
+    var key = Encoding.ASCII.GetBytes(builder.Configuration.GetSection("JwtConfig:Secret").Value);
+    jwt.SaveToken = true;
+    jwt.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        RequireExpirationTime = false
+    };
+
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
